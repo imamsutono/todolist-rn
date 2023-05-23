10be0afad44934e870122025654a90406d64ffc7
Imam Sutono <imamsutono>
2023-05-23 08:13:49 +0700

    fix status bar

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'react-native';
 import HomeScreen from './src/screens/Home/HomeScreen';
 import NewTaskScreen from './src/screens/NewTask/NewTaskScreen';
+import colors from './src/constants/colors';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor={colors.black} />
+
       <Stack.Navigator
         screenOptions={{ headerShown: false }}
       >
